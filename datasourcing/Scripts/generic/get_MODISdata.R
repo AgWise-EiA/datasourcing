babe9f27dcadaa0a2bc5ed3c03a30e6071adf0c6
Louise <louiseleroux61@gmail.com>
2024-04-09 13:27:34 +0300

    Updates
    Add the possibility to download MODIS data at different administrative levels (from country L0 up to L3)
    Improvement of the scripts and function description
    Setting the default values of paramaters

--- datasourcing/Scripts/generic/get_MODISdata.R
+++ datasourcing/Scripts/generic/get_MODISdata.R
@@ -1,49 +1,102 @@
+# Downloading of MODIS NDVI data for the Use Case
 
+# Introduction: 
+# This script allows the downloading of MODIS NDVI data (MOD13Q1 and MYD13Q1) used for the planting date exercise and crop type mapping, it allows to : 
+# (1) Preparing the environment for the downloading
+# (2) Interactive download of MODIS 
+# (3) Renaming of the MODIS file 
 
-#################################################################################################################
-## sourcing required packages 
-#################################################################################################################
+#### Getting started #######
+
+# 1. Sourcing required packages -------------------------------------------
 packages_required <- c("MODIStsp", "mapedit", "leaflet", "shiny","shinydashboard","shinyFiles",
                        "shinyalert", "rappdirs","shinyjs",
-                       "leafem", "mapedit", "magrittr")
+                       "leafem", "mapedit", "magrittr", "reticulate", "sf", "rgee", "terra",
+                       "geodata", "dplyr")
 
 installed_packages <- packages_required %in% rownames(installed.packages())
 if(any(installed_packages == FALSE)){
   install.packages(packages_required[!installed_packages])}
 
 lapply(packages_required, library, character.only = TRUE)
-#################################################################################################################
-## get MODIS layers
-#################################################################################################################
 
-pathOut = "~/agwise-datasourcing/dataops/datasourcing/Data/Global_GeoData/Landing/MODISdata" 
-user = "amitcimmyt" 
-password = "Ambica_81" 
-start_date = "2022.04.24"
-end_date = "2022.05.20" 
-country = "Rwanda"
+# load required packages
+suppressWarnings(suppressPackageStartupMessages(invisible(lapply(packages_required, library, character.only = TRUE))))
 
-## we need a mechanism to get to the shape files 
+# 2. Downloading MODIS Data -------------------------------------------
 
+download_MODIS<-function(country,useCaseName, level=0, admin_unit_name=NULL, Start_year, End_year, overwrite=FALSE){
 
-
-# MODIS_intearctive_download(start_date = "2022.04.24", end_date = "2022.05.20", 
-#                            pathOut = "~/agwise-datasourcing/dataops/datasourcing/Data/Global_GeoData/Landing/MODISdata",
-#                            user, password)
-
-MODIS_intearctive_download <- function( start_date=NULL, end_date=NULL, pathOut, user, password){
+  #' @description 
+  #' @param country country name
+  #' @param useCaseName use case name  name
+  #' @param level the admin unit level, in integer, to be downloaded -  Starting with 0 for country, then 1 for the first level of subdivision (from 1 to 3). Default is zero
+  #' @param admin_unit_name name of the administrative level to be download, default is NULL (when level=0) , else, to be specified as a vector (eg. c("Nandi"))
+  #' @param overwrite default is FALSE 
+  #' @param Start_year the first year of the period of interest in integer
+  #' @param End_year the last year of the period of interest in integer
+  #'
+  #' @return one VI layer each 8 days (both TERRA and AQUA) over the period of interest, in WGS 84 (EPSG 4326) and the result will be written out in agwise-datasourcing/dataops/datasourcing/Data/useCaseName/MODISdata/raw/
+  #'
+  #' @examples download_MODIS (country = "Kenya", useCaseName = "KALRO", level= 2, admin_unit_name = c("Butula"), Start_year = 2021, End_year = 2021, overwrite = TRUE)
+  #' 
+  #' 
+  ## 2.1. Preparing the environment ####
+  ### 2.1.1. Creating a directory to store the downloaded data ####
   
-  # countryShp <- geodata::gadm(country, level = 3, path='.')
+  pathOut <- paste("/home/jovyan/agwise-datasourcing/dataops/datasourcing/Data/useCase_", country, "_",useCaseName, "/", "MODISdata/raw/", sep="")
+  
+  if (!dir.exists(pathOut)){
+    dir.create(file.path(pathOut), recursive = TRUE)
+  }
+  
+  ### 2.1.2. Get the country boundaries ####
+  
+  # Read the relevant shape file from gdam to be used to crop the global data
+  countryShp <- geodata::gadm(country, level, path='.')
 
-  MODIStsp()
+  # Case admin_unit_name == NULL
+  if (is.null(admin_unit_name)){
+    countryShp <-countryShp
+  }
   
-  setwd("~/agwise-datasourcing/dataops/datasourcing/Data/Global_GeoData/Landing/MODISdata")
-  MODIStsp_get_prodlayers("M*D13Q1")
+  # Case admin_unit_name is not null 
+  if (!is.null(admin_unit_name)) {
+    if (level == 0) {
+      print("admin_unit_name is not null, level can't be eq. to 0 and should be set between 1 and 3")
+    }
+    if (level == 1){
+      countryShp <- subset(countryShp, countryShp$NAME_1 %in% admin_unit_name)
+    } 
+    if (level == 2){
+      countryShp <- subset(countryShp, countryShp$NAME_2 %in% admin_unit_name)
+    }
+    if (level == 3){
+      countryShp <- subset(countryShp, countryShp$NAME_3 %in% admin_unit_name)
+    }
+  }
+
+  terra::writeVector(countryShp,paste0(pathOut,"useCase_", country, "_",useCaseName,"_Boundary.shp"), overwrite=overwrite)
+  spafile <- paste0(pathOut,"/useCase_", country, "_",useCaseName,"_Boundary.shp")
+
+  ### 2.1.3. Define the downloading parameters
+  user = "amitcimmyt" 
+  password = "Ambica_81" 
+  start_date = paste0(Start_year,'.01.01')
+  end_date = paste0(End_year, '.12.31') 
+
+  ## 2.2. Interactive MODIS data download ####
+  #MODIStsp()
   
-  MODIStsp(gui             = FALSE, 
-           out_folder      = pathOut, # "C:\\GIS\\define your output folder here", ## Path to download the images
+  setwd(pathOut)
+  MODIStsp::MODIStsp_get_prodlayers("M*D13Q1")
+  
+  MODIStsp::MODIStsp(gui             = FALSE, 
+           out_folder      = pathOut, ## Path to download the images
+           out_folder_mod = pathOut,
+           selcat          = "Ecosystem Variables - Vegetation Indices",
            selprod         = "Vegetation Indexes_16Days_250m (M*D13Q1)",## define which product of MODIS images
-           bandsel         = c("EVI"), 
+           bandsel         = c("NDVI"), 
            spatmeth        = "file",
            #quality_bandsel = "QA_usef", 
            sensor         = "Both",  ## define Terra or Aqua or both, do we need Aqua??
@@ -54,57 +107,29 @@ MODIS_intearctive_download <- function( start_date=NULL, end_date=NULL, pathOut,
            start_date      = start_date, 
            end_date        = end_date, 
            verbose         = FALSE,
-           spafile = "~/agwise-datasourcing/dataops/datasourcing/Data/useCase_Rwanda_RAB/Potato/Landing/AEZ/gadm41_RWA_3.shp", ## the boundary file of the study area in .shp format
+           spafile = spafile, ## path to the boundary file of the study area in .shp format
            out_projsel = "User Defined",
            output_proj = 4326,
-           resampling = "bilinear")
+           resampling = "bilinear",
+           delete_hdf = TRUE)
   
+  ## 2.3. Rename file ####
+  # Case MOD
+  files <- list.files(path = paste0(pathOut, "useCase_", country, "_",useCaseName,"_Boundary/VI_16Days_250m_v61/NDVI/"), pattern = "MOD13Q*", full.names = TRUE)
+  new_names <- sub(pattern = "MOD13Q1", replacement = country, x = files)
+  file.rename(from = files, to = new_names)
+  
+  # Case MYD
+  files <- list.files(path = paste0(pathOut, "useCase_", country, "_",useCaseName,"_Boundary/VI_16Days_250m_v61/NDVI/"), pattern = "MYD13Q*", full.names = TRUE)
+  new_names <- sub(pattern = "MYD13Q1", replacement = country, x = files)
+  file.rename(from = files, to = new_names)
 }
 
-
-
-
-
-
-
-
-### layer by layer
-library(maptools)
-library(MODISTools)
-library(sf)
-
-data(wrld_simpl)
-world = st_as_sf(wrld_simpl)
-lux = world[world$NAME=='Luxembourg',]
-#Now we find the location (centroid) and size of the country:
-  
-  #find centroid of polygon in long-lat decimal degrees
-  lux.cent = st_centroid(lux)
-
-#find width and height of country in km
-lux.proj = st_transform(lux, 
-                        "+proj=moll +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +units=km +no_defs")
-lux.km_lr = diff(st_bbox(lux.proj)[c(1,3)])
-lux.km_ab = diff(st_bbox(lux.proj)[c(2,4)])
-#Using this info, we can download the correct Modis data (using leaf-area index, lai, as an example):
-  
-  #download the MODIS tiles for the area we defined
-
-lux_lai <- mt_subset(product = "MOD15A2H",
-                     lat = lux.cent$LAT, lon =  lux.cent$LON,
-                     band = "Lai_500m",
-                     start = "2004-01-01", end = "2004-02-01",
-                     km_lr = lux.km_lr, km_ab = lux.km_ab,
-                     site_name = "Luxembourg",
-                     internal = TRUE, progress = TRUE)
-
-# convert to a spatial raster
-lux.rast = mt_to_raster(df = lux_lai, reproject = TRUE)
-lux.rast = raster::mask(lux.rast, lux)
-plot(lux.rast)
-plot(st_geometry(lux),add=T)
-
-
-
-
+# country = "Kenya"
+# useCaseName = "Test"
+# level = 2
+# admin_unit_name = c("Budalangi", "Butula")
+# Start_year = "2021"
+# End_year = "2021"
+# overwrite = TRUE
 
--- datasourcing/Scripts/generic/get_MODISts_PreProc.R
+++ datasourcing/Scripts/generic/get_MODISts_PreProc.R
@@ -1,12 +1,15 @@
-# Input data preparation 
+# Preprocessing and smoothing of MODIS NDVI time series for the Use Case
 
 # Introduction: 
-#This script serves as a tutorial on how to work with MODIS time series datasets, pre-process them, and apply basic operations to make it analysis ready.It covers :
-#i.  Reading multi-temporal datasets and stacking them 
-#ii. Subsetting the analysis year 
-#iii.Extracting the area of interest 
-#iv. Applying smoothing techniques to fill data gaps 
-#v.  Saving the raster 
+# This script to pre-process the MODIS NDVI time series, and apply basic operations to make it analysis ready. This script has to be run
+# after get_MODISdata.R. It covers :
+# (1) - Reading multi-temporal datasets and stacking them 
+# (2) - Subsetting the analysis year 
+# (3) - Extracting the area of interest 
+# (4) - Applying smoothing techniques to fill data gaps 
+# (5) - Saving the raster 
+# NOTE : This script is reading a crop mask that is downloaded through Google Earth Engine and need to be run before running this script.
+#        Please follow the corresponding documention : get_ESACropland_fromGEE.html
 
 #### Getting started #######
 
@@ -17,39 +20,41 @@ packages_required <- c("plotly", "raster", "rgdal", "gridExtra", "sp", "ggplot2"
 installed_packages <- packages_required %in% rownames(installed.packages())
 if(any(installed_packages == FALSE)){
   install.packages(packages_required[!installed_packages])}
-
+library(SpaDES.tools)
 # load required packages
 suppressWarnings(suppressPackageStartupMessages(invisible(lapply(packages_required, library, character.only = TRUE))))
 
 # 2. Preparing and Smoothing the raster time series -------------------------------------------
 
-smooth_rasterTS<-function(country, useCaseName, Crop, Planting_year, Harvesting_year, overwrite){
+smooth_rasterTS<-function(country, useCaseName, Planting_year, Harvesting_year, overwrite = FALSE){
   
-  #' @description Function that will preprocess the MODIS EVI (8 days) time series, including a masking-out of the cropped area and a gap-filling & smoothing of the TS with a Savitzky-Golay filter. If the cropping season is on a civil year, the initial TS should have 46 images, if the cropping season is between two civil years, the initial TS should have 92 images.
+  #' @description Function that will preprocess the MODIS NDVI (8 days) time series, including a masking-out of the cropped area and a gap-filling & smoothing of the TS with a Savitzky-Golay filter. If the cropping season is on a civil year, the initial TS should have 46 images, if the cropping season is between two civil years, the initial TS should have 92 images.
   #' @param country country name
   #' @param useCaseName use case name  name
   #' @param overwrite default is FALSE 
   #' @param Planting_year the planting year in integer
   #' @param Harvesting_year the harvesting year in integer
   #'
-  #' @return raster files cropped from global data and the result will be written out in agwise-datasourcing/dataops/datasourcing/Data/useCaseName/MODISdata/transform/EVI
+  #' @return raster files cropped from global data and return a NDVI time series smoothed that is written out in agwise-datasourcing/dataops/datasourcing/Data/useCaseName/MODISdata/transform/NDVI
   #'
   #' @examples smooth_rasterTS(country = "Rwanda", useCaseName = "RAB", Planting_year = 2021, Harvesting_year = 2021, overwrite = TRUE)
   #' 
   #' 
   ## 2.1. Creating a directory to store the cropped and smoothed data ####
-  pathOut <- paste("/home/jovyan/agwise-datasourcing/dataops/datasourcing/Data/useCase_", country, "_",useCaseName, "/", "MODISdata/transform/EVI", sep="")
+  pathOut <- paste("/home/jovyan/agwise-datasourcing/dataops/datasourcing/Data/useCase_", country, "_",useCaseName, "/", "MODISdata/transform/NDVI", sep="")
   
   if (!dir.exists(pathOut)){
     dir.create(file.path(pathOut), recursive = TRUE)
   }
   
-  ## 2.2. Read  and scale the relevant raster time series ####
-  listRaster_EVI <-list.files(paste0("/home/jovyan/agwise-datasourcing/dataops/datasourcing/Data/useCase_", country, "_",useCaseName, "/","MODISdata/raw/EVI"), pattern=".tif$", full.names=T)
+  ## 2.2. Read  and scale the raster and shape data ####
+  ### 2.2.1. Get the country boundaries ####
+  # Read the relevant shape file from gdam to be used to crop the data
+  countryShp <- terra::vect(list.files(paste0("/home/jovyan/agwise-datasourcing/dataops/datasourcing/Data/useCase_", country, "_",useCaseName, "/","MODISdata/raw/"), pattern="Boundary.shp$", full.names=T))
   
+  ### 2.2.2. Get the NDVI time series ####
+  listRaster_EVI <-list.files(paste0("/home/jovyan/agwise-datasourcing/dataops/datasourcing/Data/useCase_", country, "_",useCaseName, "/","MODISdata/raw/useCase_", country, "_",useCaseName,"_Boundary/VI_16Days_250m_v61/NDVI/"), pattern=".tif$", full.names=T)
   stacked_EVI <- terra::rast(listRaster_EVI) #stack
-  stacked_EVI <- stacked_EVI/10000 ## scaling to EVI value ranges from -1 to +1
-  #plot(stacked_EVI)
   
   ## 2.3. Subsetting the year of analysis ####
   
@@ -60,10 +65,9 @@ smooth_rasterTS<-function(country, useCaseName, Crop, Planting_year, Harvesting_
     stacked_EVI_s <- stacked_EVI[as.character(Planting_year)]
     
     # check that the required numbers of files are there (1 year = 46 images)
-    
     if ( terra::nlyr(stacked_EVI_s) != nblayers){stop('The number of layers should be equal to 46!')}
-    
   }
+  
   # Case Planting Year < Harvesting Year
   if (Planting_year < Harvesting_year){
     stacked_EVI_P <- stacked_EVI[as.character(Planting_year)]
@@ -85,19 +89,29 @@ smooth_rasterTS<-function(country, useCaseName, Crop, Planting_year, Harvesting_
     stop( "Planting_year can't be > to Harvesting_year")
   }
   
+  rm(listRaster_EVI)
+  rm(stacked_EVI)
+  
   ## 2.4. Masking out of the cropped area ####
-  cropmask <-terra::rast(list.files(paste0("/home/jovyan/agwise-datasourcing/dataops/datasourcing/Data/useCase_", country, "_",useCaseName, "/","MODISdata/raw/CropMask"), pattern=".tif$", full.names = T))
-  plot(cropmask)
   
-  ## crop mask to MODIS extent 
-  terra::ext(cropmask) <- terra::ext(stacked_EVI_s)
+  ### 2.4.1. Get the cropland mask and resample to NDVI ####
+  cropmask <- list.files(paste0("/home/jovyan/agwise-datasourcing/dataops/datasourcing/Data/useCase_", country, "_",useCaseName, "/","MODISdata/raw/useCase_", country, "_",useCaseName,"_Boundary/CropMask"), pattern=".tif$", full.names=T)
+  cropmask <- terra::rast(cropmask)
+  cropmask <- terra::mask(cropmask, countryShp)
+  ## reclassification 1 = crop, na = non crop
+  m1 <- cbind(c(40), 1)
+  cropmask <- terra::classify(cropmask, m1, others=NA)
+  cropmask <- terra::resample(cropmask, stacked_EVI_s)
+  
   ## crop and mask cropland
-  stacked_EVI_s <- terra::crop(stacked_EVI_s, cropmask)
-  stacked_EVI_s <- terra::mask(stacked_EVI_s, cropmask)
-  
+  #stacked_EVI_s <- stacked_EVI_s/10000 ## scaling to NDVI value ranges from -1 to +1
+  #stacked_EVI_s <- stacked_EVI_s * cropmask
+
   ## 2.5. Applying Savitzky-Golay filter ####
-  ## create SG filter function  ##
+  ## Split the raster into 16 parts to speed the process ##
+  #y <- SpaDES.tools::splitRaster(stacked_EVI_s, nx=4,ny=4)
   
+  ## create SG filter function  ##
   # Case Planting Year = Harvesting Year
   if (Planting_year == Harvesting_year){
     start <- c(Planting_year, 1)
@@ -117,18 +131,20 @@ smooth_rasterTS<-function(country, useCaseName, Crop, Planting_year, Harvesting_
   
   fun <- function(x) {
     v=as.vector(x)
-    z<- substituteNA(v, type = "mean")
+    z<- timeSeries::substituteNA(v, type= "mean")
     MODIS.ts2 = ts(z, start=start, end=end, frequency=nblayers)
-    sg=sgolayfilt(MODIS.ts2, p=3, n=9, ts=1) # run savitzky-golay filter## edit the function if required
+    sg=signal::sgolayfilt(MODIS.ts2, p=3, n=9, ts=1) # run savitzky-golay filter## edit the function if required
   }
-  ## Apply function on data ###
+  ## Apply function on data (split data set) ###
+
   EVI_SGfil <- terra::app(x=stacked_EVI_s, fun)
+  EVI_SGfil <- EVI_SGfil*cropmask
   
   ## Rename the SG filter time series
   names(EVI_SGfil) <- names(stacked_EVI_s)
   
   ## 2.6. Saving the raster ####
-  filename <- paste0(country,'_', useCaseName, '_MODIS_EVI_', Planting_year,'_', Harvesting_year, '_SG.tif')
+  filename <- paste0(country,'_', useCaseName, '_MODIS_NDVI_', Planting_year,'_', Harvesting_year, '_SG.tif')
   terra::writeRaster(EVI_SGfil, paste(pathOut, filename, sep="/"), filetype="GTiff", overwrite=overwrite)
   
 }
