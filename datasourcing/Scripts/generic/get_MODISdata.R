df637eb6b64c3ac482053e8e99588993778f5705
Louise <louiseleroux61@gmail.com>
2024-08-19 11:48:05 +0300

    Major Update : Change of Package
    Shift from MODIStsp from modisfast to download the data

--- datasourcing/Scripts/generic/get_MODISdata.R
+++ datasourcing/Scripts/generic/get_MODISdata.R
@@ -9,10 +9,7 @@
 #### Getting started #######
 
 # 1. Sourcing required packages -------------------------------------------
-packages_required <- c("mapedit", "leaflet", "shiny","shinydashboard","shinyFiles",
-                       "shinyalert", "rappdirs","shinyjs",
-                       "leafem", "mapedit", "magrittr", "reticulate", "sf", "rgee", "terra",
-                       "geodata", "dplyr", 'ggspatial', "ggplot2")
+packages_required <- c( "sf","geodata", "dplyr", "tidyterra", "modisfast", "terra", "lubridate")
 
 installed_packages <- packages_required %in% rownames(installed.packages())
 if(any(installed_packages == FALSE)){
@@ -23,14 +20,11 @@ lapply(packages_required, library, character.only = TRUE)
 # load required packages
 suppressWarnings(suppressPackageStartupMessages(invisible(lapply(packages_required, library, character.only = TRUE))))
 
-# Specific case of MODIS package
-# Has been removed from the CRAN following the MODIS decommissioned, we load the latest version of the package locally
-library(MODIStsp, lib.loc = '~/agwise-datasourcing/dataops/datasourcing/Scripts/generic/R-Package')
 
 # 2. Downloading MODIS Data -------------------------------------------
 
 download_MODIS<-function(country,useCaseName, level=0, admin_unit_name=NULL, Start_year, End_year, overwrite=FALSE){
-
+  
   #' @description 
   #' @param country country name
   #' @param useCaseName use case name  name
@@ -48,7 +42,7 @@ download_MODIS<-function(country,useCaseName, level=0, admin_unit_name=NULL, Sta
   ## 2.1. Preparing the environment ####
   ### 2.1.1. Creating a directory to store the downloaded data ####
   
-  pathOut <- paste("/home/jovyan/agwise-datasourcing/dataops/datasourcing/Data/useCase_", country, "_",useCaseName, "/", "MODISdata/raw/", sep="")
+  pathOut <- paste("/home/jovyan/agwise-datasourcing/dataops/datasourcing/Data/useCase_", country, "_",useCaseName, "/", "MODISdata/raw/useCase_", country,"_", useCaseName, "_Boundary/VI_16Days_250m_v61", sep="")
   
   if (!dir.exists(pathOut)){
     dir.create(file.path(pathOut), recursive = TRUE)
@@ -58,7 +52,7 @@ download_MODIS<-function(country,useCaseName, level=0, admin_unit_name=NULL, Sta
   
   # Read the relevant shape file from gdam to be used to crop the global data
   countryShp <- geodata::gadm(country, level, path= pathOut)
-
+  
   # Case admin_unit_name == NULL
   if (is.null(admin_unit_name)){
     countryShp <-countryShp
@@ -79,98 +73,123 @@ download_MODIS<-function(country,useCaseName, level=0, admin_unit_name=NULL, Sta
       countryShp <- subset(countryShp, countryShp$NAME_3 %in% admin_unit_name)
     }
   }
-
-  terra::writeVector(countryShp,paste0(pathOut,"useCase_", country, "_",useCaseName,"_Boundary.shp"), overwrite=overwrite)
-  spafile <- paste0(pathOut,"/useCase_", country, "_",useCaseName,"_Boundary.shp")
-
+  
+  terra::writeVector(countryShp,paste0(pathOut,"/useCase_", country, "_",useCaseName,"_Boundary.shp"), overwrite=overwrite)
+  #spafile <- paste0(pathOut,"/useCase_", country, "_",useCaseName,"_Boundary.shp")
+  
   ### 2.1.3. Define the downloading parameters
+  ## User and Password
   user = "amitcimmyt" 
-  password = "Ambica_81" 
-  start_date = paste0(Start_year,'.01.01')
-  end_date = paste0(End_year, '.12.31') 
-
-  ## 2.2. Interactive MODIS data download ####
-  #MODIStsp()
+  password = "Ambica_81"
+  ## Login to Earthdata servers with your EOSDIS credentials. 
+  log <- mf_login(credentials = c(user,password))  # set your own EOSDIS username and password
   
+  ## Extent and time range of interest
+  roi <- st_as_sf(countryShp)
+  roi$id <- seq(1:nrow(roi))
+  
+  start_date = paste0(Start_year,'-01-05')
+  end_date = paste0(End_year, '-12-31') 
+  
+  time_range <- as.Date(c(start_date, end_date))
+ 
+  ## MODIS collections and variables of interest
+  collectionT <- "MOD13Q1.061"  #for Terra
+  collectionA <- "MYD13Q1.061"  #for Aqua
+  variables <- c("_250m_16_days_NDVI") # run mf_list_variables("MOD11A2.061") for an exhaustive list of variables available for the collection "MOD11A1.062"
+  
+  ## 2.2. MODIS data download and processing ####
   setwd(pathOut)
-  MODIStsp::MODIStsp_get_prodlayers("M*D13Q1")
   
-  MODIStsp::MODIStsp(gui             = FALSE, 
-           out_folder      = pathOut, ## Path to download the images
-           out_folder_mod = pathOut,
-           selcat          = "Ecosystem Variables - Vegetation Indices",
-           selprod         = "Vegetation Indexes_16Days_250m (M*D13Q1)",## define which product of MODIS images
-           bandsel         = c("NDVI"), 
-           spatmeth        = "file",
-           #quality_bandsel = "QA_usef", 
-           sensor         = "Both",  ## define Terra or Aqua or both, do we need Aqua??
-           #out_res_sel = "User Defined",
-           #out_res = 0.002786033, 
-           user            = user, 
-           password        = password,
-           start_date      = start_date, 
-           end_date        = end_date, 
-           verbose         = FALSE,
-           spafile = spafile, ## path to the boundary file of the study area in .shp format
-           out_projsel = "User Defined",
-           output_proj = 4326,
-           resampling = "bilinear",
-           delete_hdf = TRUE)
+  ### 2.2.1. MODIS data download ####
+  ## Get the URLs of the data 
+  #for Terra
+  urlsT <- mf_get_url(
+    collection = collectionT,
+    variables = variables,
+    roi = roi,
+    time_range = time_range
+  )
   
-  ## 2.3. Rename file ####
-  # Case MOD
-  files <- list.files(path = paste0(pathOut, "useCase_", country, "_",useCaseName,"_Boundary/VI_16Days_250m_v61/NDVI/"), pattern = "MOD13Q*", full.names = TRUE)
-  new_names <- sub(pattern = "MOD13Q1", replacement = country, x = files)
-  file.rename(from = files, to = new_names)
+  #for Aqua
+  urlsA <- mf_get_url(
+    collection = collectionA,
+    variables = variables,
+    roi = roi,
+    time_range = time_range
+  )
   
-  # Case MYD
-  files <- list.files(path = paste0(pathOut, "useCase_", country, "_",useCaseName,"_Boundary/VI_16Days_250m_v61/NDVI/"), pattern = "MYD13Q*", full.names = TRUE)
-  new_names <- sub(pattern = "MYD13Q1", replacement = country, x = files)
-  file.rename(from = files, to = new_names)
+  ## Download the data. 
+  res_terra <- mf_download_data(urlsT, parallel = T, path=pathOut) # for Terra
+  res_aqua <- mf_download_data(urlsA, parallel = T, path=pathOut) # for Aqua
   
-  ## 2.4. Create a Gif ####
-  # files <- list.files(path = paste0(pathOut, "useCase_", country, "_",useCaseName,"_Boundary/VI_16Days_250m_v61/NDVI/"), pattern = ".tiff*", full.names = TRUE)
-  # modis <- terra::rast(files)
-  # 
-  # # Define the limits
-  # ndvi.min <- min(modis, na.rm=FALSE)
-  # ndvi.min <- min(terra::values(ndvi.min), na.rm=TRUE)
-  # 
-  # ndvi.max <- max(modis, na.rm=FALSE)
-  # ndvi.max <- max(terra::values(ndvi.max), na.rm=TRUE)
-  # 
-  # lims <- c(ndvi.min, ndvi.max)
-  # 
-  # country_sf <- sf::st_as_sf(countryShp)
-  # 
-  # for (i in 1:len(names(modis))){
-  #   name <- names(modis)[i]
-  #   p <- ggplot() +
-  #     geom_spatraster(data = modis[[i]]) +
-  #     scale_fill_hypso_c(palette = "dem_screen", na.value = "transparent", trans='reverse', name="NDVI")+ theme_bw()+
-  #     theme(legend.position = "right")+ 
-  #     geom_sf(data=country_sf, fill=NA, color="white", linewidth=0.5)+
-  #     coord_sf(expand = FALSE)+
-  #     xlab("Longitude")+ ylab("Latitude") + labs(title= paste0(country, '-', useCaseName, " from ", Planting_year, " to ", Harvesting_year),
-  #                   subtitle = name, caption = "MODIS NDVI, 250-m spatial res and 8-days temporal res" )+
-  #     annotation_scale(style='bar', location='bl')+annotation_north_arrow(which_north = "true", location='tr', height=unit(1, 'cm'), width=unit(1, 'cm'))
-  #   map_name <- paste0(pathOut, "useCase_", country, "_",useCaseName,"_Boundary/VI_16Days_250m_v61/NDVI/", name, "_animate.png")
-  #   ggsave(map_name, p, width=8, height = 5.10)
-  #  
-    #https://www.youtube.com/watch?v=GMnuNjnXnS8
-    #https://dieghernan.github.io/tidyterra/reference/scale_hypso.html
-    
-  #}
+  ### 2.2.2. Process the MODIS data ####
+  
+  ## Copy past all the files in the same directory
+  # Create a new directory
+  dir.create(file.path(pathOut, "NDVI/MOD13Q1"), showWarnings = FALSE, recursive=T) # for Terra
+  dir.create(file.path(pathOut, "NDVI/MYD13Q1"), showWarnings = FALSE, recursive=T) # for Aqua
+  from.dir <- paste0(pathOut, '/data')
+  to.dir   <- paste0(pathOut,'/NDVI')
+  
+  filesT    <- list.files(path = from.dir, pattern=collectionT, full.names = TRUE, recursive = TRUE) # for Terra
+  for (f in filesT) file.copy(from = f, to = paste0(to.dir,'/MOD13Q1'))
+  
+  filesA    <- list.files(path = from.dir, pattern=collectionA, full.names = TRUE, recursive = TRUE) # for Aqua
+  for (f in filesA) file.copy(from = f, to = paste0(to.dir,'/MYD13Q1'))
+
+  ## Import, Merge and Reproject MODIS data with Terra
+  
+  modis_terra <- mf_import_data(
+    path = paste0(to.dir,'/MOD13Q1'),
+    collection = collectionT, 
+    proj_epsg = 4326
+  ) #for Terra
+  
+  modis_aqua <- mf_import_data(
+    path = paste0(to.dir,'/MYD13Q1'),
+    collection = collectionA, 
+    proj_epsg = 4326
+  ) #for Aqua
+  
+  ## Rename MODIS layer
+  # Convert date into DOY
+  doy_terra <- yday(time(modis_terra)) #for Terra
+  doy_aqua <- yday(time(modis_aqua)) #for Aqua
+  
+  # Convert DOY into a 3 digits number
+  doy_terra <- sprintf("%03d", doy_terra)
+  doy_aqua <- sprintf("%03d", doy_aqua)
+  
+  # Convert date into Year
+  year_terra <- year(time(modis_terra)) #for Terra
+  year_aqua <- year(time(modis_aqua)) #for Aqua
+  
+  # Set names
+  names(modis_terra) <- paste0(country,'_NDVI_',doy_terra,'_', year_terra) #for Terra
+  names(modis_aqua) <- paste0(country,'_NDVI_',doy_aqua,'_', year_aqua) #for Aqua
+  
+  ### 2.2.3. Save the MODIS data ####
+  terra::writeRaster(modis_terra, filename=paste0(to.dir, '/',names(modis_terra), ".tif"), overwrite=TRUE)
+  terra::writeRaster(modis_aqua, filename=paste0(to.dir, '/',names(modis_aqua), ".tif"), overwrite=TRUE)
+  
+  #### 2.2.4. Clear the folder ####
   
   ## Delete the GDAM folder
   unlink(paste0(pathOut, '/gadm'), force=TRUE, recursive = TRUE)
+  
+  ## Delete the data folder
+  unlink(paste0(pathOut, '/data'), force=TRUE, recursive = TRUE)
+  
+  ## Delete MOD/MY folder
+  unlink(paste0(to.dir,'/MOD13Q1'), force=TRUE, recursive=TRUE)
+  unlink(paste0(to.dir,'/MYD13Q1'), force=TRUE, recursive=TRUE)
+  
+  ## Delete the .json file
+  unlink(list.files(to.dir, pattern=".json", full.names = T))
+  
 }
 
-# country = "Kenya"
-# useCaseName = "Test"
-# level = 2
-# admin_unit_name = c("Budalangi", "Butula")
-# Start_year = "2021"
-# End_year = "2021"
-# overwrite = TRUE
 
+ 
+ 
